0ac8a7eb79a704374f7888ecbab17802b2d01754
Žanas Stundys <z.stundys@gmail.com>
2024-01-20 13:07:09 +0200

    Update HTML and CSS in index.html and canvas.ts

--- src/components/canvas.ts
+++ src/components/canvas.ts
@@ -1,24 +1,30 @@
 import { raise } from "@/lib/raise";
+import styles from './canvas.css?inline';
 
 export class MlCanvas extends HTMLElement {
   #canvas = document.createElement('canvas')
   #ctx = this.#canvas.getContext('2d') ?? raise('Could not get canvas context');
 
+  shadowRoot = this.attachShadow({ mode: 'open' });;
+
   constructor() {
     super();
-    this.attachShadow({ mode: 'open' });
-    this.shadowRoot?.appendChild(this.#canvas);
+
+    this.shadowRoot.appendChild(this.#canvas);
+
+    const style = document.createElement('style');
+    style.textContent = styles;
+    this.shadowRoot.appendChild(style);
 
     this.#canvas.width = 800;
     this.#canvas.height = 600;
-    
+
     this.#ctx.fillStyle = 'red';
     this.#ctx.fillRect(0, 0, this.#canvas.width, this.#canvas.height);
   }
 
+
   connectedCallback() {
-    console.log('Connected!');
-    
   }
 }
 
