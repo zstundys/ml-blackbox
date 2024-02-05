4a75ea79a3396df79dd1d9154df18092379d5c1a
Žanas Stundys <z.stundys@gmail.com>
2024-02-05 23:33:52 +0200

    13:33

--- src/components/canvas.ts
+++ src/components/canvas.ts
@@ -1,30 +1,34 @@
 import { raise } from "@/lib/raise";
-import styles from './canvas.css?inline';
+import styles from "./canvas.css?inline";
 
 export class MlCanvas extends HTMLElement {
-  #canvas = document.createElement('canvas')
-  #ctx = this.#canvas.getContext('2d') ?? raise('Could not get canvas context');
+  #canvas = document.createElement("canvas");
+  #ctx = this.#canvas.getContext("2d") ?? raise("Could not get canvas context");
 
-  shadowRoot = this.attachShadow({ mode: 'open' });;
+  shadowRoot = this.attachShadow({ mode: "open" });
 
   constructor() {
     super();
 
     this.shadowRoot.appendChild(this.#canvas);
 
-    const style = document.createElement('style');
+    const style = document.createElement("style");
     style.textContent = styles;
     this.shadowRoot.appendChild(style);
 
     this.#canvas.width = 800;
     this.#canvas.height = 600;
 
-    this.#ctx.fillStyle = 'red';
+    this.#ctx.fillStyle = "white";
     this.#ctx.fillRect(0, 0, this.#canvas.width, this.#canvas.height);
   }
 
-
   connectedCallback() {
+    this.addEventListener("mousedown", (event) => {
+      const rect = this.#canvas.getBoundingClientRect();
+      const mouse = [event.clientX - rect.left, event.clientY - rect.top];
+
+      console.log(mouse);
+    });
   }
 }
-
